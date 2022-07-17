31b2e48037194746159fd91394d69dbba417c1bf
Devanshi Panchal <devanshipanchal.12701@gmail.com>
2022-07-17 18:42:17 +0530

    added routing

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Stack,Avatar,Typography} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+
 const UserCard = ({item:{firstName,lastName,id}}) => {
+    const navigate = useNavigate()
   return (
-   <Stack direction="row"
+   <Stack 
+   className="usercard"
+   direction="row"
    spacing={2}
-   sx={{py:1}}>
+   sx={{py:1}}
+   onClick={()=>navigate(`/${id}/${firstName} ${lastName}`)}>
        <Avatar 
        src={`https://avatars.dicebear.com/api/initials/${firstName}${lastName}.svg`}
        sx={{width:"32px",height:"32px"}} />
--- client/src/pages/HomeScreen.js
+++ client/src/pages/HomeScreen.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
 import {Box} from '@mui/material'
+import {Route,Routes} from 'react-router-dom'
+import Welcome from '../components/Welcome'
+import ChatScreen from '../components/ChatScreen'
+
+
+const AllRoutes = ()=> {
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome/>} />
+            <Route path="/:id/:name" element={<ChatScreen/>} />
+        </Routes>
+    )
+}
 const HomeScreen = () => {
   return (
-    <Box>
+    <Box
+    display="flex">
         <SideBar/>
+        <AllRoutes />
     </Box>
   )
 }
