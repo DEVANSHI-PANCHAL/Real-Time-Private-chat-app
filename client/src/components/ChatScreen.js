44950cca8b68336d852846656c6e7401258dd19c
Devanshi Panchal <devanshipanchal.12701@gmail.com>
2022-07-18 10:58:40 +0530

    chat screen ui

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { AppBar, Toolbar, Box, Avatar, Typography,TextField } from '@mui/material'
+import MessageCard from '../components/MessageCard'
 
 const ChatScreen = () => {
     const {id,name} = useParams()
   return (
-    <div>{id} {name}</div>
+   <Box 
+   flexGrow={1}>
+       <AppBar position="static"
+       sx={{backgroundColor:"white",boxShadow:0}}>
+           <Toolbar>
+           <Avatar 
+       src={`https://avatars.dicebear.com/api/initials/${name}.svg`}
+       sx={{width:"32px",height:"32px",mr:2}} />
+           <Typography variant="h6" color="black">{name}</Typography>
+           </Toolbar>
+       </AppBar>
+       <Box backgroundColor="#f5f5f5"
+       height="80vh"
+       padding="10px"
+      sx={{ overflowY:"auto"}}>
+           <MessageCard text="Hi devanshi" date="123" direction="start"/>
+           <MessageCard text="Hi debu" date="123" direction="end"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+           <MessageCard text="Hi bans" date="123" direction="start"/>
+       </Box>
+       <TextField
+       placeholder="Enter a message"
+       variant="standard"
+       fullWidth
+       multiline
+       rows={2}></TextField>
+   </Box>
   )
 }
 
---
+++ client/src/components/MessageCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AppBar, Toolbar, Box, Avatar, Typography,TextField } from '@mui/material'
+
+const MessageCard = ({text,date,direction}) => {
+  return (
+      <Box
+      display="flex"
+      justifyContent={direction}>
+          <Box>
+        <Typography
+        variant="subtitle2"
+        backgroundColor="white"
+        padding="5px"
+        >
+            {text}
+        </Typography>
+        <Typography
+        variant="caption"
+        
+        padding="5px"
+        >
+            {date}
+        </Typography>
+    </Box>
+      </Box>
+    
+  )
+}
+
+export default MessageCard
